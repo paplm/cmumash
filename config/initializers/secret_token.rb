db4368c5a08cbc4cbeec41cb6c1e676875b16530
Pipatpon Kessuwan <pretaget@gmail.com>
2011-10-28 00:47:33 +0700

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmumash::Application.config.secret_token = 'afc5453be3915397142a1aa791e4eaa5ab51f94def26eb52429f53f7eaf3f022fa0f9b8a7bd3aadd204380ba0642784bd17836a88ccaf3b9288373b9233a48fb'
